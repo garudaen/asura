89a23f39f85f9e27997450b40d803ce7c820e449
张富贵 <zhenhua.zhang@b-and-qchina.com>
2020-11-27 15:31:17 +0800

    access token

--- feishu/go.mod
+++ feishu/go.mod
@@ -1,3 +1,9 @@
 module asura/feishu
 
 go 1.15
+
+require (
+	github.com/franela/goreq v0.0.0-20171204163338-bcd34c9993f8
+	github.com/garudaen/beluga v0.0.0-20201118070621-a277c2980beb
+	github.com/json-iterator/go v1.1.10
+)
--- feishu/notify.go
+++ feishu/notify.go
@@ -1,1 +1,42 @@
 package feishu
+
+import (
+	"asura/feishu/core/body"
+	"asura/feishu/core/urls"
+
+	"github.com/franela/goreq"
+	jsoniter "github.com/json-iterator/go"
+)
+
+//"github.com/garudaen/asura/goreq"
+// @Title  飞书发送消息模块
+// @Description  飞书发送消息模块
+// @Author  张振华  2020-10-31
+// @Update  张振华  2020-10-31
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary //jsoniter 初始化
+//Auth 授权
+type Auth struct {
+	AppID             string `json:"app_id"`
+	UserID            string `json:"user_id"`
+	AppSecret         string `json:"app_secret"`
+	AppAccessToken    string `json:"app_access_token"`
+	TenantAccessToken string `json:"tenant_access_token"`
+	Authorization     string `json:"authorization"`
+}
+
+//GetAccessToken 获取 access token
+func (auth *Auth) GetAccessToken() *body.AppAccessTokenInternalRespBody {
+	postBody, _ := json.Marshal(auth)
+	res := &body.AppAccessTokenInternalRespBody{}
+	req, _ := goreq.Request{
+		Method:      "POST",
+		Uri:         urls.APIAppAccessTokenInternal,
+		Body:        postBody,
+		ContentType: "application/json",
+	}.Do()
+	req.Body.FromJsonTo(&res)
+	auth.AppAccessToken = res.AppAccessToken
+	auth.TenantAccessToken = res.TenantAccessToken
+	return res
+}
